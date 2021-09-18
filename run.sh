2358a6f137027003a165c24298eae0768a570513
harisahmed05 <ahsanharisahmed@gmail.com>
2021-09-18 23:22:13 +0600

    we can just compile the main file :/

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # I'll add makefiles/cmake later
-gcc src/main.c src/headers/ezbsh.h src/headers/ezbsh.h -lreadline -o ezbsh
+gcc src/main.c -lreadline -o ezbsh
 ./ezbsh
 # If it's already compiled then why again? I'll fix them later.
 # Attention: libreadline-dev required!!!
