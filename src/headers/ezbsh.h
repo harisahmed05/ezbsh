af1ba6f0bbc07c6b3c01a2937ea9d7ded531ae17
harisahmed05 <ahsanharisahmed@gmail.com>
2021-09-20 20:30:26 +0600

    no more including same header multiple times

--- src/headers/ezbsh.h
+++ src/headers/ezbsh.h
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <readline/readline.h>
+#include <sys/wait.h>
 
 char **get_input(char *input);
 int cd(char *path);
--- src/main.c
+++ src/main.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <readline/readline.h>
-#include <sys/wait.h>
-
 #include "headers/ezbsh.c"
 
 int main()
